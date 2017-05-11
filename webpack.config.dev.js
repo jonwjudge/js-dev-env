62d1802ff3304dfdb766adc99dc2dea0a372338b
jonwjudge <jjudge@TPCHD.LOCAL>
2017-05-11 11:40:27 -0700

    added webpack

---
+++ webpack.config.dev.js
@@ -0,0 +1,35 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json']
+  },
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: false,
+      debug: true,
+      noInfo: true // set to false to see a list of every file being bundled.
+    }),
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loaders: ['babel-loader']},
+      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+    ]
+  }
+};
